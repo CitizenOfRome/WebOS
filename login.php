a28e9b9f231acae5c2f3a8a65be0aad0ef43ee75
Sathvik Ponangi <psathvik@gmail.com>
2012-01-17 11:04:55 +0530

    TheOSumCloud!

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$con = mysql_connect("db01-share", "Custom App-24766", "FrogKissesTheQueen", true);	if(!($con && mysql_select_db("OS", $con)))	die("DATABASE ERROR");
+	$con = mysql_connect("db01-share", "Custom App-24766", "FrogKissesTheQueen", true);	if(!($con && mysql_select_db("os_phpfogapp_com", $con)))	die("DATABASE ERROR");
 	$userName = mysql_real_escape_string(strtolower($_POST["username"]));
 	$passWord = md5($_POST["password"]);
 	$result = mysql_fetch_row(mysql_query("SELECT userName FROM Users WHERE userName='".$userName."' AND passWord='".$passWord."'", $con));
